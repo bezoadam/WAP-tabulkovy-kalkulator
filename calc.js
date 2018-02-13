67271c8d6529da3843afa3e86031c7bd584ac297
Adam Bezak <bezoadam95@gmail.com>
2018-02-13 23:28:19 +0100

    implement single cell highlight on tap, cell selection with keyboard

--- calc.js
+++ calc.js
@@ -1,12 +1,13 @@
-function tableCreate(rowCount, cellCount){
+function tableCreate(rowCount, colCount, tableId){
     var body = document.body,
         tbl  = document.createElement('table');
     tbl.style.width  = '100px';
     tbl.style.border = '1px solid black';
+    tbl.id = tableId;
 
     for(var i = 0; i < rowCount; i++){
         var tr = tbl.insertRow();
-        for(var j = 0; j < cellCount; j++){
+        for(var j = 0; j < colCount; j++){
             var td = tr.insertCell();
             td.appendChild(document.createTextNode('Cell'));
             td.style.border = '1px solid black';
@@ -14,3 +15,102 @@ function tableCreate(rowCount, cellCount){
     }
     body.appendChild(tbl);
 }
+
+function highlight_cells() {
+    var all = document.getElementsByTagName("td");
+    for (var i=0;i<all.length;i++) {
+        all[i].onclick = inputClickHandler;       
+    }
+}
+
+function deselect_cells() {
+    var all = document.getElementsByTagName("td");
+    for (var i=0;i<all.length;i++) {
+        all[i].style.borderWidth = '1px';   
+        all[i].style.borderColor = 'black';
+        all[i].id = '';
+    }
+}
+
+function inputClickHandler(e) {
+    deselect_cells()
+    e = e || window.event;
+    var tdElm = e.target||e.srcElement;
+    tdElm.style.borderWidth = '2px';   
+    tdElm.style.borderColor = 'blue';
+    tdElm.id = 'selected';
+    if(tdElm.style.backgroundColor == 'rgb(255, 0, 0)') {
+        tdElm.style.backgroundColor = '#fff';
+    } else {
+        tdElm.style.backgroundColor = '#f00';
+    }
+}
+
+function checkKey(e) {
+    e = e || window.event;
+    var index = null
+
+    switch (e.keyCode) {
+        case 38:
+            index = getNextTableCellIndex('up')
+            break;
+        case 40:
+            index = getNextTableCellIndex('down')
+            break;
+        case 37:
+            index = getNextTableCellIndex('left')
+            break;
+        case 39:
+            index = getNextTableCellIndex('right')
+            break;
+        default:
+            console.log('err')
+    }
+
+    selectNextTableCell(index)
+}
+
+function selectNextTableCell(index) {
+    var table = document.getElementById('table');
+    var tdElm = table.rows[index[0]].cells[index[1]];
+    if (tdElm == null) {
+        console.log('out of table');
+        return;
+    }
+    deselect_cells();
+    tdElm.style.borderWidth = '2px';   
+    tdElm.style.borderColor = 'blue';
+    tdElm.id = 'selected';    
+}
+
+function getNextTableCellIndex(arrow) {
+    var table = document.getElementById('table');
+    var tdElm = document.getElementById('selected');
+    var rowIndex = tdElm.closest('tr').rowIndex;
+    var colIndex = tdElm.closest('td').cellIndex;
+    console.log('row: ' + rowIndex);
+    console.log('col: ' + colIndex);
+    var index = [];
+
+    switch (arrow) {
+        case 'up':
+            index = [rowIndex - 1, colIndex];
+            break;
+        case 'down':
+            index = [rowIndex + 1, colIndex];
+            break;
+        case 'left':
+            index = [rowIndex, colIndex - 1];
+            break;
+        case 'right':
+            index = [rowIndex, colIndex + 1];
+            break;
+        default:
+            console.log('err');
+    }
+
+    return index;
+}
+
+window.onload = highlight_cells;
+window.onkeydown = checkKey;
