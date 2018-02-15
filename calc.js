aa6e6572c396d006ca3b2c4a3358f9d43e7515fe
Adam Bezak <bezoadam95@gmail.com>
2018-02-15 22:42:51 +0100

    implement more cells selection

--- calc.js
+++ calc.js
@@ -13,14 +13,15 @@ function tableCreate(rowCount, colCount, tableId){
     body.appendChild(tbl);
 }
 
-function highlight_cells() {
+function highlightCells() {
     var all = document.getElementsByTagName("td");
     for (var i=0;i<all.length;i++) {
-        all[i].onclick = inputClickHandler;       
+        all[i].onclick = inputClickHandler;
+        all[i].ondblclick = inputDoubleClickHandler;
     }
 }
 
-function deselect_cells() {
+function deselectCells() {
     var all = document.getElementsByTagName("td");
     for (var i=0;i<all.length;i++) {
         all[i].id = '';
@@ -28,17 +29,23 @@ function deselect_cells() {
     }
 }
 
-function inputClickHandler(e) {
-    deselect_cells();
+function inputDoubleClickHandler(e) {
     e = e || window.event;
     var tdElm = e.target||e.srcElement;
-    tdElm.id = 'selected';
-    tdElm.setAttribute('contenteditable', 'true');
     if(tdElm.style.backgroundColor == 'rgb(255, 0, 0)') {
         tdElm.style.backgroundColor = '#fff';
     } else {
         tdElm.style.backgroundColor = '#f00';
-    }
+    }    
+}
+
+function inputClickHandler(e) {
+    deselectCells();
+    e = e || window.event;
+    var tdElm = e.target||e.srcElement;
+    tdElm.id = 'focused';
+    tdElm.setAttribute('contenteditable', 'true');
+    tdElm.focus();
 }
 
 function checkKey(e) {
@@ -79,15 +86,15 @@ function selectNextTableCell(index) {
         console.log('out of table');
         return;
     }
-    deselect_cells();
-    tdElm.id = 'selected';
+    deselectCells();
+    tdElm.id = 'focused';
     tdElm.setAttribute('contenteditable', 'true');
     tdElm.focus();
 }
 
 function getNextTableCellIndex(arrow) {
     var table = document.getElementById('table');
-    var tdElm = document.getElementById('selected');
+    var tdElm = document.getElementById('focused');
     var rowIndex = tdElm.closest('tr').rowIndex;
     var colIndex = tdElm.closest('td').cellIndex;
     console.log('row: ' + rowIndex);
@@ -114,5 +121,68 @@ function getNextTableCellIndex(arrow) {
     return index;
 }
 
-window.onload = highlight_cells;
+function selectingCells() {
+    var isMouseDown = false;
+    var startRowIndex = null;
+    var startCellIndex = null;
+    var endRowIndex = null;
+    var endCellIndex = null;
+
+    var table = document.getElementById('table');
+    table.addEventListener('mousedown', function(e) {
+        isMouseDown = true;
+        startRowIndex = e.target.parentElement.rowIndex;
+        startCellIndex = e.target.cellIndex;
+    }, false)
+
+    table.addEventListener('mouseover', function(e) {
+        // console.log(e.target.parentElement.rowIndex);
+        // console.log(e.target.cellIndex);
+    }, false)
+
+    table.addEventListener('mouseup', function(e) {
+        if (isMouseDown) {
+            endRowIndex = e.target.parentElement.rowIndex;
+            endCellIndex = e.target.cellIndex;
+            if (startRowIndex != endRowIndex || startCellIndex != endCellIndex) {
+                calculateSelection();
+            }
+        }
+        isMouseDown = false;
+    }, false)
+
+
+    function calculateSelection() {
+        var rowStart, rowEnd, cellStart, cellEnd;
+        
+        if (endRowIndex < startRowIndex) {
+            rowStart = endRowIndex;
+            rowEnd = startRowIndex;
+        } else {
+            rowStart = startRowIndex;
+            rowEnd = endRowIndex;
+        }
+        
+        if (endRowIndex < startCellIndex) {
+            cellStart = endCellIndex;
+            cellEnd = startCellIndex;
+        } else {
+            cellStart = startCellIndex;
+            cellEnd = endCellIndex;
+        }        
+
+        for (var i = rowStart; i <= rowEnd; i++) {
+            for (var j = cellStart; j <= cellEnd; j++) {
+                var tdElm = table.rows[i].cells[j];
+                tdElm.classList.add('selected');
+            }        
+        }
+    }
+
+
+}
+
+
+window.onload = highlightCells;
+window.onload = selectingCells;
 window.onkeydown = checkKey;
